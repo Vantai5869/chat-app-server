58b9b2592672df4e48c68eb8dbff018fb28d250e
sucwin <taipv2x@gmail.com>
2021-10-08 07:43:06 +0700

    clear user controller 2

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -100,6 +100,7 @@ const getAllUsers = async(req: Request, res: Response, next: NextFunction) => {
                 count: users.length
             });
         }
+        return res.status(400)
     } catch (error) {
         return res.status(500).json({
             message: error.message,
