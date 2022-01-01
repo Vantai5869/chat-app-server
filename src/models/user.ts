0904c1656063f365f090fd5f27f4ea0e2f1d972f
sucwin <taipv2x@gmail.com>
2022-01-01 22:30:32 +0700

    clear spare in field email of user

--- src/models/user.ts
+++ src/models/user.ts
@@ -28,7 +28,6 @@ class IUser {
     @prop({
         required: false,
         trim:true ,
-        sparse:true,
         match: /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })
     public email: string;  
     
