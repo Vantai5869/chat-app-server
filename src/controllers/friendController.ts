2caa62fa30e3af0d0b3c3b5d486cd2a636402374
Phạm Văn Tài_D3 <taipv@arrowhitech.biz>
2022-03-04 15:24:20 +0700

    Fdsafsd

--- src/controllers/friendController.ts
+++ src/controllers/friendController.ts
@@ -129,14 +129,12 @@ const editFriend = async (req, res) => {
 }
 
 const cancelFriend = async (req, res) => {
-    let kq;
+    let kq=true;
     FriendModel.findOneAndDelete({ friendId: req.params.friend_id, userId: req.params.user_id },
         (err, doc) => {
             if (err) {
                 console.log("Something wrong when updating data!");
                 kq= false
-            }else{
-                kq=true;
             }
         });
     FriendModel.findOneAndDelete({ userId: req.params.friend_id, friendId: req.params.user_id },
@@ -144,8 +142,6 @@ const cancelFriend = async (req, res) => {
             if (err) {
                 console.log("Something wrong when updating data!");
                 kq= false
-            }else{
-                kq=true;
             }
         });
 
